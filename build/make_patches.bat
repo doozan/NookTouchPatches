87beff6a45c829a6f9d8b0b07e043fec2767e96e
Jeff Doozan <github@doozan.com>
2012-11-17 14:27:59 -0500

    Updated patches

--- build/make_patches.bat
+++ build/make_patches.bat
@@ -1,9 +1,22 @@
 @ECHO OFF
 
+set VER=1.1.5
+
+cd "%~dp0\smali\%VER%\android.policy"
+call git diff base  > ..\android.policy.patch
+"%~dp0\bin\unix2dos" ..\android.policy.patch
+
+cd "%~dp0\smali\%VER%\services"
+call git diff base  > ..\services.patch
+"%~dp0\bin\unix2dos" ..\services.patch
+
+
 set VER=1.2.0
 
 cd "%~dp0\smali\%VER%\android.policy"
 call git diff base  > ..\android.policy.patch
+"%~dp0\bin\unix2dos" ..\android.policy.patch
 
 cd "%~dp0\smali\%VER%\services"
 call git diff base  > ..\services.patch
+"%~dp0\bin\unix2dos" ..\services.patch
